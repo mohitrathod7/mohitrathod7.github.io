41295b26f05b7ebd978f24b672f593e0e3c8da78
Mohit Rathod <57721715+mohitrathod7@users.noreply.github.com>
2024-01-16 14:21:03 -0500

    Add files via upload

---
+++ portfolio/assets/js/script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function sociallink(url){
+    window.open(url, '_blank').focus()
+}
+
+function open_certificate(url){
+    window.open(url, '_blank').focus()
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
